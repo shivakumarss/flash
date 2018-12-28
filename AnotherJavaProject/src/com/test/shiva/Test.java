1f5258ff3baf68a7888451b7526b79f6230fd79a
hduser <ss.shivakumar1@gmail.com>
2018-12-28 14:42:55 +0530

    a

--- AnotherJavaProject/src/com/test/shiva/Test.java
+++ AnotherJavaProject/src/com/test/shiva/Test.java
@@ -4,11 +4,10 @@ public class Test {
 
 	public static void main(String[] args) {
 		System.out.println("This is test file. ");
-<<<<<<< HEAD
+
 		// Exit 11
-		
+
 		System.exit(-1);
-=======
->>>>>>> refs/remotes/origin/master
 	}
+
 }
